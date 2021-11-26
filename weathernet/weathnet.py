f8bd97a3caa5c50ae579d236fe8269145c81e4a0
russellyq <russellyq2333@gmail.com>
2021-11-26 16:41:44 +0800

    b commit

--- weathernet/weathnet.py
+++ weathernet/weathnet.py
@@ -7,8 +7,4 @@ import torch.nn.functional as F
 class _LiLaBlock(nn.Module):
     def __init__(self):
         super(_LiLaBlock, self).__init__()
-        self.conv1 = nn.Conv2d(1, 7, 3)
-        self.conv2 = nn.Conv2d(1, 3, 3)
-        self.conv3 = nn.Conv2d(1, 3, 3, dilation=2)
-        self.conv4 = nn.Conv2d(1, 3, 7)
         
