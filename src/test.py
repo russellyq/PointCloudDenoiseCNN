12f7c1f423882c57c2b89c6cf0e7d4ed1338a84b
russellyq <russellyq2333@gmail.com>
2021-12-06 16:23:23 +0800

    t commit

--- src/test.py
+++ src/test.py
@@ -20,6 +20,9 @@ if torch.cuda.is_available():
 else:
     DEVICE = torch.device('cpu')
 
+result_path= './result_test.txt'
+if os.path.exists(result_path):
+    os.remove(result_path)
 
 def main(opt):
 
@@ -30,14 +33,15 @@ def main(opt):
 
     # network
     model = WeatherNet()
-    model.load_state_dict('../checkpoints/saved_model.pth')
+
+    checkpoint = torch.load('../checkpoints/saved_model_0.pth')
+    model.load_state_dict(checkpoint)
 
     if torch.cuda.device_count() > 1:
         model = nn.DataParallel(model)
     print("Let's use", torch.cuda.device_count(), "GPUs!")
 
     model.to(DEVICE)
-
     model.eval()
 
     test_label_true = torch.LongTensor()
@@ -50,29 +54,32 @@ def main(opt):
 
             predictions = model(images)
 
-            labels = labels.cpu().detach().numpy()
-            predictions = predictions.argmax(dim=1).squeeze().data.cpu().detach().numpy()
+            # labels = labels.cpu().detach().numpy()
+            # predictions = predictions.argmax(dim=1).squeeze().data.cpu().detach().numpy()
+            # predictions = predictions.astype('int')
+            labels = labels.cpu()
+            predictions = predictions.argmax(dim=1).squeeze().data.cpu()
+
+            test_label_true = torch.cat((test_label_true, labels), dim=0)
+            test_label_pred = torch.cat((test_label_pred, predictions), dim=0)
 
             evaluator = Evaluator(3)
-            evaluator.add_batch(labels, predictions.astype('int'))
-            Acc = evaluator.Pixel_Accuracy()
-            Acc_class = evaluator.Pixel_Accuracy_Class()
-            mIoU = evaluator.Mean_Intersection_over_Union()
-            FWIoU = evaluator.Frequency_Weighted_Intersection_over_Union()
-
-            print("Acc:{}, Acc_class:{}, mIoU:{}, fwIoU: {}".format(Acc, Acc_class, mIoU, FWIoU))
-
-            val_label_true = torch.cat((test_label_true, labels), dim=0)
-            val_label_pred = torch.cat((test_label_pred, predictions), dim=0)
-
-        val_acc, val_acc_cls, val_mean_iu, val_fwavacc=label_accuracy_score(val_label_true.numpy(), val_label_pred.numpy(), 3)
-    print('acc:{:.4f}, acc_cls:{:.4f}, mean_iu:{:.4f}, fwavacc:{:.4f}'.format(val_acc, val_acc_cls, val_mean_iu, val_fwavacc))
+            evaluator.add_batch(labels.detach().numpy(), predictions.detach().numpy().astype('int'))
+              
+        Acc = evaluator.Pixel_Accuracy()
+        Acc_class = evaluator.Pixel_Accuracy_Class()
+        mIoU = evaluator.Mean_Intersection_over_Union()
+        FWIoU = evaluator.Frequency_Weighted_Intersection_over_Union()
+        print("Acc:{}, Acc_class:{}, mIoU:{}, fwIoU: {}".format(Acc, Acc_class, mIoU, FWIoU))
+        
+        val_acc, val_acc_cls, val_mean_iu, val_fwavacc = label_accuracy_score(test_label_true.numpy(), test_label_pred.numpy(), 3)
+        print('acc:{:.4f}, acc_cls:{:.4f}, mean_iu:{:.4f}, fwavacc:{:.4f}'.format(val_acc, val_acc_cls, val_mean_iu, val_fwavacc))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--batch_size', type=int, default=32, help='input batch size')
+        '--batch_size', type=int, default=20, help='input batch size')
     parser.add_argument(
         '--epochs', type=int, default=1, help='number of epochs to train for')
     
--- src/train_segmentation.py
+++ src/train_segmentation.py
@@ -11,6 +11,8 @@ from weathnet import WeatherNet
 from utils import mIoU
 from torch.autograd import Variable
 from torchvision.transforms import transforms
+import datetime
+
 
 os.environ["CUDA_VISIBLE_DEVICES"]="0, 1, 2"
 
@@ -23,7 +25,11 @@ result_path= './result.txt'
 if os.path.exists(result_path):
     os.remove(result_path)
 
+
 def main(opt):
+    with open(result_path, 'a') as f:
+        f.write(''.format(datetime.datetime.now()))
+        f.close()
 
     # lodar dataset & dataloader
     train_dataset = DeNoiseDataset(mode='train')
@@ -44,11 +50,14 @@ def main(opt):
 
     # loss function
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=4e-18, betas=(0.9, 0.999), eps=1e-8)
 
     min_valid_loss = np.inf
 
-    for epoch in range(opt.epochs):
+    LEARNING_RATE =  opt.learning_rate
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, betas=(0.9, 0.999), eps=1e-8)
+    
+    for epoch in range(opt.epochs):  
+        
         train_loss = 0.0
         valid_loss = 0.0
         
@@ -70,9 +79,9 @@ def main(opt):
                 print("epoch:%d, %d/%d, train_loss:%0.3f" % (epoch+1, i+1, (len(train_dataset) - 1) // opt.batch_size + 1, loss.cpu().item()*labels.size(0)))
         
         train_loss /= len(train_dataset)
-        print('\nepoch:{}, train_loss:{:.4f}'.format(epoch+1, train_loss))
+        print('\nepoch:{}, train_loss:{:.4f}\n'.format(epoch+1, train_loss))
         with open(result_path, 'a') as f:
-            f.write('\n epoch:{}, train_loss:{:.4f}'.format(epoch+1, train_loss))
+            f.write('\n epoch:{}, train_loss:{:.4f}\n'.format(epoch+1, train_loss))
             f.close()
 
         # validation
@@ -90,39 +99,23 @@ def main(opt):
                 print("epoch:%d, %d/%d, val_loss:%0.3f" % (epoch+1, i+1, (len(val_dataset) - 1) // opt.batch_size + 1, loss.cpu().item()*labels.size(0)))
 
         valid_loss /= len(val_dataset)
-        print('\nepoch:{}, val_loss:{:.4f}'.format(epoch+1, valid_loss))
+        print('\nepoch:{}, val_loss:{:.4f}\n'.format(epoch+1, valid_loss))
         with open(result_path, 'a') as f:
-            f.write('\n epoch:{}, val_loss:{:.4f}'.format(epoch+1, valid_loss))
+            f.write('\n epoch:{}, val_loss:{:.4f}\n'.format(epoch+1, valid_loss))
             f.close()
         
         if min_valid_loss > valid_loss:
-            min_valid_loss = valid_loss
+            
             print(f'Validation Loss Decreased({min_valid_loss:.6f}--->{valid_loss:.6f}) \t Saving The Model')
-            save_name = '../checkpoints/saved_model_' +str(epoch) + '_.pth'
-            torch.save(model.state_dict(), save_name)
+            save_name = '../checkpoints/saved_model_' +str(epoch) + '.pth'
+            torch.save(model.module.state_dict(), save_name)
+            min_valid_loss = valid_loss
+        
 
-        # # test
-        # clear_mIoU, rain_mIoU, fog_mIoU = 0.0, 0.0, 0.0
-        # number = 0
-        # with torch.no_grad():
-        #     for i, data in enumerate(test_dataloader, 0):
-        #         images, labels = data
-        #         images, labels = images.to(DEVICE, dtype=torch.float), labels.to(DEVICE, dtype=torch.long)
-
-        #         predictions = model(images)
-
-        #         labels = labels.cpu().detach().numpy()
-        #         predictions = predictions.cpu().detach().numpy()
-
-        #         for prediction, label in zip(predictions, labels):
-        #             clear_mIoU += mIoU(prediction, label, 'clear')
-        #             rain_mIoU += mIoU(prediction, label, 'rain')
-        #             fog_mIoU += mIoU(prediction, label, 'fog')
-        #             number += 1
-
-        # clear_mIoU, rain_mIoU, fog_mIoU = clear_mIoU / number, rain_mIoU / number, fog_mIoU / number
-        # print('[%d] acc: %.3f, %.3f, %.3f' % (epoch + 1, clear_mIoU, rain_mIoU, fog_mIoU))
-
+# LEARNING_RATE:
+# LiLaNet: 4e-4
+# RangeNet: 1e-4
+# WeatherNet: 8e-4
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -130,8 +123,9 @@ if __name__ == "__main__":
         '--batch_size', type=int, default=20, help='input batch size')
     parser.add_argument(
         '--epochs', type=int, default=50, help='number of epochs to train for')
-    
-
+    parser.add_argument(
+        '--learning_rate', type=float, default=1e-3)
     opt = parser.parse_args()
+    print(opt)
     main(opt)
 
